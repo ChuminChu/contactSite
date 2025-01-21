81bc5c01ad9e24b4900d0c88329cfbc2975e45b8
chumin-yeong <chu35my@gmail.com>
2025-01-21 15:02:11 +0900

    initial commit

---
+++ src/main/java/contactSite/ContactSiteApplication.java
@@ -0,0 +1,13 @@
+package contactSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/contactSite/ContactSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package contactSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
