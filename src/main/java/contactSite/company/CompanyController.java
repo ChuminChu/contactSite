797cb600cfc59536a879f2e0088ff4c13486efa8
jun <mmy519@naver.com>
2025-01-24 12:05:21 +0900

    fix :경로 설정 / 회원가입 - CompanyMypageResponse/ 목록조회 - requestparm / 상세조회 - CompanyMypageResponse / 회원정보수정 - CompanyMypageResponse, CompanyMypageRequest / 비밀번호 수정 - CompanyPasswordRequest

--- src/main/java/contactSite/company/CompanyController.java
+++ src/main/java/contactSite/company/CompanyController.java
@@ -1,8 +1,8 @@
 package contactSite.company;
 
+import contactSite.Field;
 import contactSite.LoginUtils.LoginMember;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,46 +10,57 @@ import java.util.List;
 @RestController
 public class CompanyController {
 
-    private final CompanyRepository companyRepository;
     private final CompanyService companyService;
 
 
-    public CompanyController(CompanyRepository companyRepository, CompanyService companyService) {
-        this.companyRepository = companyRepository;
+    public CompanyController(CompanyService companyService) {
         this.companyService = companyService;
     }
 
 
     //회원가입
-    @PostMapping
-    public void create(@Valid @RequestBody CreateCompanyRequest request){
-        companyService.create(request);
+    @PostMapping("/companys")
+    public CompanyMypageResponse create(@Valid @RequestBody CreateCompanyRequest request){
+       return companyService.create(request);
     }
 
-    //목록 조회
-    @GetMapping
-    public List<CompanyResponse> findAll(){
-        return companyService.findAll();
+
+    //간단 조회(목록조회)
+    @GetMapping("/companys")
+    public List<CompanyResponse> findAll(@RequestParam List<Field> field,
+                                         @RequestParam String address){
+        return companyService.findAll(field,address);
     }
 
-    //상세조회
-    @GetMapping
-    public CompanyDetailResponse detail(@LoginMember String authorization){
-        return companyService.findDetail(authorization);
+    //상세조회 - 기업
+    @GetMapping("/companys/{companyId}")
+    public CompanyDetailResponse findByCompany(@PathVariable String companyId){
+        return companyService.findById(companyId);
     }
 
-    //회원정보수정
-    @PutMapping
-    public Company update(@RequestBody CreateCompanyRequest request, @LoginMember String authorization){
+
+    //상세조회 - 내 정보
+    @GetMapping("/companys/my")
+    public CompanyMypageResponse detail(@LoginMember String authorization){
+        return companyService.findMypage(authorization);
+    }
+
+    //회원정보 수정
+    @PutMapping("/companys/my")
+    public CompanyMypageResponse update(@RequestBody CompanyMypageRequest request,
+                          @LoginMember String authorization){
         return companyService.update(request,authorization);
     }
 
     //비밀번호 수정
-    public Company password(@RequestBody CreateCompanyRequest request, @LoginMember String authorization ){
-        return companyService.PWupdate(request, authorization);
+    @PatchMapping("/companys/my")
+    public void password(@RequestBody CompanyPasswordRequest request,
+                            @LoginMember String authorization ){
+        companyService.updatePassword(request, authorization);
     }
 
     //회원 탈퇴
+    @DeleteMapping("/companys/my")
     public void delete(@LoginMember String authorization){
         companyService.delete(authorization);
     }
