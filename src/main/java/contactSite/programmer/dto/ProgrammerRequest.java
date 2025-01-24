2e80094240a2d34abeaa4872b8a287c8f8aba2f8
jun <mmy519@naver.com>
2025-01-24 17:16:02 +0900

    fix :validation 추가

--- src/main/java/contactSite/programmer/dto/ProgrammerRequest.java
+++ src/main/java/contactSite/programmer/dto/ProgrammerRequest.java
@@ -1,15 +1,19 @@
 package contactSite.programmer.dto;
 
 import contactSite.Field;
+import jakarta.validation.constraints.*;
 
 public record ProgrammerRequest(
+        @NotBlank @Size(min = 6, max = 20)
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영어와 숫자만 가능합니다. /6자리 이상")
         String userId,
-        String name,
-        int age,
-        String email,
-        int personalHistory,
+
+        @Size(min = 1, max = 20) @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "이름은 알파벳과 한글만 가능합니다.")String name,
+        @NotBlank @PositiveOrZero @Max(100) int age,
+        @Email String email,
+        @Max(100) int personalHistory,
         Field fieldName,
-        String selfIntroduction,
-        String certificate
+        @Size(max = 10000) String selfIntroduction,
+        @Size(max = 10000) String certificate
 ) {
 }
