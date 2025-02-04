e476bb421c3d3ae0eeac4205229c8a775c03448b
jun <mmy519@naver.com>
2025-02-04 11:46:17 +0900

    feat : birthdate 추가 body : localdate 타입

--- src/main/java/contactSite/programmer/dto/ProgrammerResponse.java
+++ src/main/java/contactSite/programmer/dto/ProgrammerResponse.java
@@ -2,10 +2,13 @@ package contactSite.programmer.dto;
 
 import contactSite.Field;
 
+import java.time.LocalDate;
+
 public record ProgrammerResponse(
         String id,
         String userId,
         String name,
+        LocalDate birthDate,
         int age,
         String email,
         int personalHistory,
