0a2658b4d4e08e8e19ba16fdeba83460f254d770
jun <mmy519@naver.com>
2025-02-04 11:44:37 +0900

    fix : age int타입 변경

--- src/main/java/contactSite/programmer/dto/read/ProgrammerDetailResponse.java
+++ src/main/java/contactSite/programmer/dto/read/ProgrammerDetailResponse.java
@@ -1,10 +1,13 @@
 package contactSite.programmer.dto.read;
 
 import contactSite.Field;
+import jakarta.validation.constraints.Past;
+
+import java.time.LocalDate;
 
 public record ProgrammerDetailResponse(
         String name,
-        Number age,
+        int age,
         String email,
         Number personalHistory,
         Field fieldName,
