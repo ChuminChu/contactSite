087b2ae16bce44d6f1bd7fb2d262b3cf6d087acd
jun <mmy519@naver.com>
2025-02-04 11:45:48 +0900

    fix : age -> birthdate로 변경 body : localdate 타입

--- src/main/java/contactSite/programmer/dto/ProgrammerRequest.java
+++ src/main/java/contactSite/programmer/dto/ProgrammerRequest.java
@@ -3,13 +3,15 @@ package contactSite.programmer.dto;
 import contactSite.Field;
 import jakarta.validation.constraints.*;
 
+import java.time.LocalDate;
+
 public record ProgrammerRequest(
         @NotBlank @Size(min = 6, max = 20)
         @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영어와 숫자만 가능합니다. /6자리 이상")
         String userId,
 
         @Size(min = 1, max = 20) @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "이름은 알파벳과 한글만 가능합니다.")String name,
-        @NotBlank @PositiveOrZero @Max(100) int age,
+        @Past(message = "YYYY-MM-DD") LocalDate birthDate,  //생년월일 넣으면 나이로 변환
         @Email String email,
         @Max(100) int personalHistory,
         Field fieldName,
