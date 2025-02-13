423537b8942de78b2c46a8b019135679678228e5
jun <mmy519@naver.com>
2025-02-13 18:02:25 +0900

    fix : 버튼 추가

--- frontend/src/app/index/CopList.tsx
+++ frontend/src/app/index/CopList.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import {useState} from "react";
 import {CompanyList} from "@/components/companyList";
+import {PageButtons} from "@/components/page-buttons";
 
 interface Company {
     id: string;
@@ -9,9 +13,9 @@ interface Company {
     likeCount: number;
 }
 
-export default async function CopList() {
-    const response = await fetch('http://localhost:8080/companies?');
-    const companies = await response.json()
+export default function CopList(props) {
+    const [companies, setCompanies] = useState<Company[]>(props.companies);
+
     return (
         <div className="container">
             <header>
@@ -19,6 +23,7 @@ export default async function CopList() {
                 <p></p>
             </header>
             <CompanyList companies={companies}/>
+            <PageButtons totalPage={100} currentPage={2} setCompanies={setCompanies}/>
         </div>
     );
 }
