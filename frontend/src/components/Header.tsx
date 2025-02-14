ea5bb94b7d9d95bb6f7b25c2772ed3d325dac2c0
jun <mmy519@naver.com>
2025-02-14 17:57:19 +0900

    fix : 링크 삭제

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,52 +2,54 @@
 
 //import React, { useState } from "react";
 import Link from "next/link";
-import { FaSignInAlt, FaUser, FaComment, FaBuilding } from "react-icons/fa";
-import { getUserType, setUserType } from "@/app/userTypeUtils";
+import {FaSignInAlt, FaUser, FaComment, FaBuilding} from "react-icons/fa";
+import {getUserType, setUserType} from "@/app/userTypeUtils";
 
 export default async function Header() {
-  await setUserType(); // 서버에서 userType 설정
-  const userType = getUserType(); // 전역 userType 가져오기
+    await setUserType(); // 서버에서 userType 설정
+    const userType = getUserType(); // 전역 userType 가져오기
 
-  //const authState: string = "notLoggedIn";
-  return (
-    <div className="hdrWrap">
-      <header>
-        <h1 className="logo">
-          <Link href="/">LOGOHERE</Link>
-        </h1>
-        <ul className="nav">
-          <li>
-            <Link href="/developerList">개발자 목록</Link>
-          </li>
-          <li>
-            <Link href="/companyList">기업 목록</Link>
-          </li>
-        </ul>
-        <div className="utilBox">
-          {userType === "notLoggedIn" && (
-            <>
-              <Link href={"/login"}>
-                <FaSignInAlt />
-              </Link>
-            </>
-          )}
-          {(userType === "developer" || userType === "businessmen") && (
-            <>
-              <Link href={""}>
-                <FaComment />
-              </Link>
-              <Link href={""}>
-                {userType === "developer" ? (
-                  <FaUser />
-                ) : userType === "businessmen" ? (
-                  <FaBuilding />
-                ) : null}
-              </Link>
-            </>
-          )}
+    //const authState: string = "notLoggedIn";
+    return (
+        <div className="hdrWrap">
+            <header>
+                <h1 className="logo">
+                    <Link href="/">LOGOHERE</Link>
+                </h1>
+                <ul className="nav">
+                    <li>
+                        <Link href="/developerList">개발자 목록</Link>
+                    </li>
+                    <li>
+                        <Link href="/companyList">기업 목록</Link>
+                    </li>
+                </ul>
+                <div className="utilBox">
+                    {userType === "notLoggedIn" && (
+                        <>
+                            <Link href={"/login"}>
+                                <FaSignInAlt/>
+                            </Link>
+                        </>
+                    )}
+                    {(userType === "developer" || userType === "businessmen") && (
+                        <>
+                            <Link href={""}>
+                                <FaComment/>
+                            </Link>
+
+                            {userType === "developer" ? (
+
+                                <FaUser/>
+                            ) : userType === "businessmen" ? (
+                                <Link href={"/companyMypage"}>
+                                    <FaBuilding/>
+                                </Link>
+                            ) : null}
+                        </>
+                    )}
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
