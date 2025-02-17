704433535ef26e948175e1111bc82d5e1e74742f
jun <mmy519@naver.com>
2025-02-17 17:36:11 +0900

    fix : function 수정

--- frontend/src/app/index/CopList.tsx
+++ frontend/src/app/index/CopList.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import {useState} from "react";
 import {CompanyList} from "@/components/companyList";
+import {PageButtons} from "@/components/page-buttons";
 
 interface Company {
     id: string;
@@ -9,9 +13,11 @@ interface Company {
     likeCount: number;
 }
 
-export default async function CopList() {
-    const response = await fetch('http://localhost:8080/companies?');
-    const companies = await response.json()
+
+export default function CopList(props: { companyList: { companies: Company[]; totalPage: number; currentPage: number } }) {
+    const [companies, setCompanies] = useState<Company[]>(props.companyList.companies);
+    const [page, setPage] = useState({totalPages: 0, currentPage: 1});
+
     return (
         <div className="container">
             <header>
@@ -19,6 +25,11 @@ export default async function CopList() {
                 <p></p>
             </header>
             <CompanyList companies={companies}/>
+            <PageButtons totalPage={page.totalPages}
+                         currentPage={page.currentPage}
+                         setCompanies={setCompanies}
+                         setPage={setPage}
+            />
         </div>
     );
 }
--- frontend/src/components/companyList.tsx
+++ frontend/src/components/companyList.tsx
@@ -1,31 +1,55 @@
 import styles from "@/app/companyList/page.module.css";
-import {CompanyPage} from "@/app/companyList/page";
+import {Company} from "@/app/companyList/page";
+import Link from "next/link";
 
-export function CompanyList(props) {
+
+
+export function CompanyPage(company: Company) {
     return (
-        <table className={styles.table}>
-            <thead>
-            <tr className={styles.item}>
-                <th>기업명</th>
-                <th>구인분야</th>
-                <th>직원수</th>
-                <th>기업소재지</th>
-                <th>좋아요</th>
-            </tr>
-            </thead>
+        <tr>
+            <td>
+                <Link className="dev" href={`/companyProfile/${company.id}`}>
+                    {company.name}
+                </Link>
+            </td>
+            <td>{company.field}</td>
+            <td>{company.employeeCount}</td>
+            <td>{company.address}</td>
+            <td>{company.likeCount}</td>
+        </tr>
 
-            <tbody>
-            {props.companies.map(company =>
-                <CompanyPage
-                    key={company.id}
-                    id={company.id}
-                    name={company.name}
-                    field={company.field}
-                    employeeCount={company.employeeCount}
-                    address={company.address}
-                    likeCount={company.likeCount}/>)}
-            </tbody>
+    )
+}
 
-        </table>
+export function CompanyList(props: { companies: Company[] }) {
+    console.log(props.companies);
+    return (
+        <section>
+            <table className={styles.table}>
+                <thead>
+                <tr className={styles.item}>
+                    <th>기업명</th>
+                    <th>구인분야</th>
+                    <th>직원수</th>
+                    <th>기업소재지</th>
+                    <th>좋아요</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                {props.companies.map(company =>
+                    <CompanyPage
+                        key={company.id}
+                        id={company.id}
+                        name={company.name}
+                        field={company.field}
+                        employeeCount={company.employeeCount}
+                        address={company.address}
+                        likeCount={company.likeCount}/>)}
+                </tbody>
+            </table>
+        </section>
+
+
     )
 }
