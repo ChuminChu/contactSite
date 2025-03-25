1113f7dff7c92e53656797d7876a5ca783f73408
chumin-yeong <chu35my@gmail.com>
2025-03-25 17:52:24 +0900

    repository는 db에 접근하려고 하려고 하기 때문에 test용 db로 다시 설정해야함

--- src/test/java/contactSite/programmerTest/ProgrammerQueryRepositoryTest.java
+++ src/test/java/contactSite/programmerTest/ProgrammerQueryRepositoryTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
+//db에 접근하려고 한다.
+@ActiveProfiles("test")
 @SpringBootTest
 public class ProgrammerQueryRepositoryTest {
 
+    //리포지토리는 db에 접근하는 거니까 이 테스트는 db가 꼭 있어야한다. 근데 ci는 독립된 리눅스?에서 하니까 당연히 db를 안된다 test용 db가 있어야한다.
     @Autowired
     ProgrammerQueryRepository programmerQueryRepository;
 
